c1aa08b23a18385c6db7d756a2d0cf0bb7a9e57a
Samuel Maina <samuelmayna@gmail.com>
2023-08-07 13:32:01 +0000

    Model one function  and runs.sh with run api

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -1,10 +1,9 @@
 # imports
 import os
 import csv
-import joblib
 import sys
-from rdkit import Chem
-from rdkit.Chem.Descriptors import MolWt
+from zinc-similarity import  100_closest 
+
 
 # parse arguments
 input_file = sys.argv[1]
@@ -13,28 +12,25 @@ output_file = sys.argv[2]
 # current file directory
 root = os.path.dirname(os.path.abspath(__file__))
 
-# checkpoints directory
-checkpoints_dir = os.path.abspath(os.path.join(root, "..", "..", "checkpoints"))
 
-# read checkpoints (here, simply an integer number: 42)
-ckpt = joblib.load(os.path.join(checkpoints_dir, "checkpoints.joblib"))
 
-# model to be run (here, calculate the Molecular Weight and add ckpt (42) to it)
-def my_model(smiles_list, ckpt):
-    return [MolWt(Chem.MolFromSmiles(smi))+ckpt for smi in smiles_list]
-    
 # read SMILES from .csv file, assuming one column with header
 with open(input_file, "r") as f:
     reader = csv.reader(f)
-    next(reader) # skip header
+    next(reader)  # skip header
     smiles_list = [r[0] for r in reader]
-    
+
 # run model
-outputs = my_model(smiles_list, ckpt)
+outputs = 100_closest(smiles_list)
+
+#check input and output have the same lenght
+input_len = len(smiles_list)
+output_len = len(outputs)
+assert input_len == output_len
 
 # write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["value"]) # header
+    writer.writerow(["similar smiles"])  # header
     for o in outputs:
-        writer.writerow([o])
+        writer.writerow(o)
--- model/framework/code/zinc-similarity.py
+++ model/framework/code/zinc-similarity.py
@@ -3,34 +3,30 @@ import requests
 import json
 import sys
 
-# Input Parameters
-input_file = open(sys.argv[1], 'r')
-Lines = input_file.readlines()[1:]
-
 fp        = 'ECFP4'
 maxCount  = '100'
 maxDist   = '20'
 
-data = []
-for input_smiles in Lines:
-    input_smiles = input_smiles.strip() 
-    url = 'http://130.92.106.217:8080/MCSS/search.jsp?maxCount=' + maxCount + '&type=numOfMols&maxDist=' + maxDist + '&group1=Princeton&group1=Enamine&group1=Otava&group1=Vitas-M&group1=Specs&group1=Urosy&group1=ChemDiv&group1=ChemBridge&group1=Others&pdbID=&mask=0&smi=' + input_smiles + '&fp=' + fp + '&searchMethod=searchByNum&searchLimit=100&retBooleans=false%3Bfalse%3Bfalse%3Bfalse%3Bfalse%3Bfalse&retVals=0%3B0%3B0%3B0%3B0%3B0&formulaCheck=null&PDB_LIGANDS=&PDB_LIGANDS_smi='
 
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, features = 'html.parser')
-    results = soup.find_all('textarea')
-    
-    T = str(results[2]).splitlines()
-    T = [i for i in T if not ('ZINC' not in i)]
-    smiles_list = []
-    similarity_indices = []
-    for i in T:
-        x = i.split(" ZINC")[0]
-        x2 = i.split(" ")[2]
-        smiles_list.append(x) 
-        similarity_indices.append(x2)
+def 100_closest(smiles_list):
+    data = []
+    for input_smiles in smiles_list:
+        input_smiles = input_smiles.strip() 
+        url = 'https://multi-fpb.gdb.tools/MCSS/search.jsp?maxCount=' + maxCount + '&type=numOfMols&maxDist=' + maxDist + '&group1=Princeton&group1=Enamine&group1=Otava&group1=Vitas-M&group1=Specs&group1=Urosy&group1=ChemDiv&group1=ChemBridge&group1=Others&pdbID=&mask=0&smi=' + input_smiles + '&fp=' + fp + '&searchMethod=searchByNum&searchLimit=100&retBooleans=false%3Bfalse%3Bfalse%3Bfalse%3Bfalse%3Bfalse&retVals=0%3B0%3B0%3B0%3B0%3B0&formulaCheck=null&PDB_LIGANDS=&PDB_LIGANDS_smi='
+
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, features = 'html.parser')
+        results = soup.find_all('textarea')
         
-    data+= [[smiles_list, similarity_indices]]
+        T = str(results[2]).splitlines()
+        T = [i for i in T if not ('ZINC' not in i)]
+        smiles_list = []
+        for i in T:
+            x = i.split(" ZINC")[0]
+            smiles_list.append(x) 
+            
+        data+= [smiles_list]
+        return data
 
-with open(sys.argv[2], 'w') as f:
-    json.dump(data, f)
+
+
