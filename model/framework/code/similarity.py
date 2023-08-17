e5f1d85ce466843d46b8bf3ff6a86a72dda9aa0f
Samuel Maina <samuelmayna@gmail.com>
2023-08-17 08:35:58 +0300

    Put working scrap url

--- model/framework/code/similarity.py
+++ model/framework/code/similarity.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
-import json
-import sys
+import urllib
+import time
 
 fp        = 'ECFP4'
 maxCount  = '100'
@@ -12,11 +12,12 @@ def zinc_100_closest(smiles_list):
     data = []
     for input_smiles in smiles_list:
         input_smiles = input_smiles.strip() 
-        url = 'https://multi-fpb.gdb.tools/MCSS/search.jsp?maxCount=' + maxCount + '&type=numOfMols&maxDist=' + maxDist + '&group1=Princeton&group1=Enamine&group1=Otava&group1=Vitas-M&group1=Specs&group1=Urosy&group1=ChemDiv&group1=ChemBridge&group1=Others&pdbID=&mask=0&smi=' + input_smiles + '&fp=' + fp + '&searchMethod=searchByNum&searchLimit=100&retBooleans=false%3Bfalse%3Bfalse%3Bfalse%3Bfalse%3Bfalse&retVals=0%3B0%3B0%3B0%3B0%3B0&formulaCheck=null&PDB_LIGANDS=&PDB_LIGANDS_smi='
+        url_encoded_smiles = urllib.parse.quote(input_smiles)
+        url = "https://multi-fpb.gdb.tools/pfpZinc.NoJava/search.jsp?maxCount="+maxCount+ "&type=numOfMols&maxDist="+maxDist+ "&group2=None&inputMol=&mask=0&smi="+url_encoded_smiles+ "&searchMode=searchByNum&limit=100"
+        print(url)
         r = requests.get(url)
         soup = BeautifulSoup(r.text, features = 'html.parser')
         results = soup.find_all('textarea')
-        
         T = str(results[0]).splitlines() if results else []
         T = [i for i in T if not ('ZINC' not in i)]
         smiles_list = []
@@ -24,6 +25,7 @@ def zinc_100_closest(smiles_list):
             x = i.split(" ZINC")[0]
             smiles_list.append(x) 
         data+= [smiles_list]
+        time.sleep(6)
     return data
 
 
